12dba6d4d6f43846c39875eccedcf39dc36eccb3
Kamlesh Nagware <kamlesh.nagware@yahoo.com>
2017-03-24 15:26:45 +0530

    Add files via upload

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -22,8 +22,8 @@ package main
 import (
 	"errors"
 	"fmt"
-	"strconv"
-	"encoding/json"
+//	"strconv"
+//	"encoding/json"
 //  "time"
 //	"strings"
 
@@ -52,32 +52,19 @@ func main() {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
 }
-// ============================================================================================================================
-// Init - reset all the things
-// ============================================================================================================================
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	var Aval int
 	var err error
 
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	if len(args) != 2 {
+	   return nil, errors.New("Incorrect number of arguments. Expecting 1")
 	}
-
-	// Initialize the chaincode
-	Aval, err = strconv.Atoi(args[0])
-	if err != nil {
-		return nil, errors.New("Expecting integer value for asset holding")
-	}
-
-	// Write the state to the ledger
-	err = stub.PutState("abc", []byte(strconv.Itoa(Aval)))				//making a test var "abc", I find it handy to read/write to it right away to test the network
-	if err != nil {
-		return nil, err
-	}
+    //Write the User Id "mail Id" arg[0] and password arg[1]
+	userid := args[0]															//argument for UserID
+	password := args[1]  	//argument for password
+	str := `{"userid": "` + userid+ `", "password": "` + password + `"}`
 	
-	var empty []string
-	jsonAsBytes, _ := json.Marshal(empty)								//marshal an emtpy array of strings to clear the index
-	err = stub.PutState("HI", jsonAsBytes)
+	err = stub.PutState(userid, []byte(str))								//Put the userid and password in blockchain
+	
 	if err != nil {
 		return nil, err
 	}
@@ -114,34 +101,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("query is running " + function)
 
-	// Handle different functions
-	if function == "read" {													//read a variable
-		return t.read(stub, args)
-	}
-	fmt.Println("query did not find func: " + function)						//error
-
-	return nil, errors.New("Received unknown function query")
-}
-
-// ============================================================================================================================
-// Read - read a variable from chaincode state
-// ============================================================================================================================
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var id, jsonResp string
-	var err error
-
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
-	}
-
-	id = args[0]
-	valAsbytes, err := stub.GetState(id)									//get the var from chaincode state
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + id + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-
-	return valAsbytes, nil													//send it onward
+	return nil, nil
 }
 
 // ============================================================================================================================
